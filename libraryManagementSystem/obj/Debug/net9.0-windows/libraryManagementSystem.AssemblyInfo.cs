cdf0d8dc34978bd1c765c54aaeb9db82166fee96
Mahmoud Ahmed <mahmoudahmed18840@gmail.com>
2025-03-03 23:44:18 +0200

    merge common branch

--- libraryManagementSystem/obj/Debug/net9.0-windows/libraryManagementSystem.AssemblyInfo.cs
+++ libraryManagementSystem/obj/Debug/net9.0-windows/libraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("libraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8507ec2b546bcf72bc5ea8e1700efaa423ad3e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f88221fdc402fab5881640403e8b16a1648cf952")]
 [assembly: System.Reflection.AssemblyProductAttribute("libraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("libraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
