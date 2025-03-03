db719df7a271e84e8640803e8bf49bfcda9211cd
Mahmoud Ahmed <mahmoudahmed18840@gmail.com>
2025-03-04 00:27:05 +0200

    merge conflicts

--- libraryManagementSystem/Program.cs
+++ libraryManagementSystem/Program.cs
@@ -1,3 +1,4 @@
+using libraryManagementSystem.Forms.CommonFroms;
 using libraryManagementSystem.Forms.Librarian;
 
 namespace libraryManagementSystem
@@ -13,7 +14,7 @@ namespace libraryManagementSystem
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new ManageBooksForm());
+            Application.Run(new ManageHomeForm());
         }
     }
 }
