53fc51a273bc59b13aac36717162596d638aa3a9
Safia-Kamal <safiakamal552@gmail.com>
2025-03-04 00:01:05 +0200

    user commit

---
+++ libraryManagementSystem/Forms/Member/ViewBooksForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using libraryManagementSystem.Models;
+
+namespace libraryManagementSystem.Forms.Member
+{
+    public partial class ViewBooksForm : Form
+    {
+        public ViewBooksForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewBooksForm_Load(object sender, EventArgs e)
+        {
+            LoadBooks();
+        }
+
+        private void LoadBooks()
+        {
+            using (var dbContext = new LibraryDbContext())
+            {
+                var books = dbContext.Books.ToList();
+                dgvBooks.DataSource = books;
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadBooks();
+
+        }
+    }
+}
