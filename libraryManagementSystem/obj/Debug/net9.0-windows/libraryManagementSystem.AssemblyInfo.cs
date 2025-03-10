bbefd41d6061ca9f6348336e5a1fdaa886fbf248
Mahmoud Ahmed <mahmoudahmed18840@gmail.com>
2025-03-11 01:18:05 +0200

    metadata

--- libraryManagementSystem/obj/Debug/net9.0-windows/libraryManagementSystem.AssemblyInfo.cs
+++ libraryManagementSystem/obj/Debug/net9.0-windows/libraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("libraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3175bdf332d60289d6914e3d8b6b161ba35fd713")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0125544bc1487ca04260482ad50bb0f9b0ec21f")]
 [assembly: System.Reflection.AssemblyProductAttribute("libraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("libraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
